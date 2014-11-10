6d6d3f28c49ae8f3cdb89ff1177c81705c1239b2
Aaron Mortenson <aamortenson1@gmail.com>
2014-11-10 01:03:38 -0500

    shows stuff

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask,request,url_for,redirect,render_template
+import urllib2, json
+
+app=Flask(__name__)
+
+@app.route("/", methods = ["GET", "POST"])
+def main():
+    foods = []
+    url = "http://api.yummly.com/v1/api/recipes?_app_id=9bb0bd30&_app_key=9e7a1eeeae374a6f14d388e755204848&flavor.sweet.min=0.8&flavor.sweet.max=1&flavor.piquant.min=0&flavor.piquant.max=0.2&requirePictures=true"
+    request = urllib2.urlopen(url)
+    res_string = request.read()
+    results = json.loads(res_string)
+    print results
+    for match in results['matches']:
+        if 'smallImageUrls' in match.keys():
+            foods+=(match['smallImageUrls'])
+    return render_template("base.html", foods=foods)
+
+
+if __name__=="__main__":
+   app.debug=True
+   app.run() 
