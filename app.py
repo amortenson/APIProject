dfbceeb95edae75566dd0122783c6d2205d5bfc2
Aaron Mortenson <aamortenson1@gmail.com>
2014-11-11 21:34:02 -0500

    now gives a list of foods

--- app.py
+++ app.py
@@ -6,16 +6,21 @@ app=Flask(__name__)
 @app.route("/", methods = ["GET", "POST"])
 def main():
     foods = []
-    url = "http://api.yummly.com/v1/api/recipes?_app_id=9bb0bd30&_app_key=9e7a1eeeae374a6f14d388e755204848&flavor.sweet.min=0.8&flavor.sweet.max=1&flavor.piquant.min=0&flavor.piquant.max=0.2&requirePictures=true"
-    request = urllib2.urlopen(url)
-    res_string = request.read()
-    results = json.loads(res_string)
-    print results
-    for match in results['matches']:
-        if 'smallImageUrls' in match.keys():
-            foods+=(match['smallImageUrls'])
-    attribution=(results['attribution']['html'])
-    return render_template("base.html", foods=foods, attribution=attribution)
+    foods .append({'flavors': [0.16666666666666666, 0.16666666666666666, 0.5, 0.5, 0.8333333333333334, 0.8333333333333334], 'image': 'http://lh3.ggpht.com/-E7i4PyfgWJJC86wxhveIHfoHCgSOv3X_DZWqhUMpFPyJ9ekC52P14utMUdwycJeSGcbepqulf0c9G3aSArVFg=s90', 'name': "Mel's Red Hot Baked Wings"})
+    foods.append({'flavors': [0.16666666666666666, 0.16666666666666666, 0.8333333333333334, 0.8333333333333334, 0.8333333333333334, 0.3333333333333333], 'image': 'http://lh5.ggpht.com/lSY2GG8Bcnl1Yxi-SAGMZZJU3xubiRNioCXiGmDYwZsK3PaYx7eRZZq1W5c7xVAgAsiMyY-Eqh9vTIEa11Yk4L0=s90', 'name': 'Jalepeno-Cheddar Bread'})
+    foods.append({'flavors': [1.0, 0.3333333333333333, 0.5, 1.0, 0.0, 0.5], 'image': 'http://lh3.ggpht.com/KXYD08_3wKQ4lpnA_dlUJepU5MQssvMxjC8_M-lQ-vq0FtNAmasbfk7qV-yhh2PCzXAPZ0pdx4gXsg9Ph1zu7ks=s90', 'name': 'Double-Chocolate Brownies'})
+    foods.append({'flavors': [0.8333333333333334, 0.16666666666666666, 0.5, 0.16666666666666666, 0.0, 0.6666666666666666], 'image': 'http://lh3.ggpht.com/VxetWazyJ4YUOjy8p9qhS7K5-VJABJ8jN1aXQAgSZgUMH5eBm3SMuCal1NM93NA3XBzAYzdIAK8Yu7zJhOij=s90', 'name': 'Red Velvet Cake'})
+    foods.append({'flavors': [0.3333333333333333, 0.8333333333333334, 0.8333333333333334, 0.8333333333333334, 0.8333333333333334, 0.6666666666666666], 'image': 'http://lh3.ggpht.com/QS5Lual_Lm5XfwdrQx4PVJ6bk0pVWC1D672pe3rjRYS7otl2AlGta4cObB32J6Ok061cAepR9qVzU-Vy5WNy=s90', 'name': 'Chile Rellenos Bake'})
+    foods.append({'flavors': [1.0, 1.0, 1.0, 0.6666666666666666, 0.0, 0.8333333333333334], 'image': 'http://lh3.ggpht.com/A1KtKWFGQ2CGAcE1dgpS5jfgvRZhl_c-G5l7KsiFWzit6VOPvKvxZG7odXi52ocDZyR7S6N7KBL2TZU_SSUCHg=s90', 'name': 'Strawberry Shortcake'})
+    foods.append({'flavors': [0.6666666666666666, 0.6666666666666666, 0.8333333333333334, 0.6666666666666666, 0.0, 0.5], 'image': 'http://lh3.ggpht.com/Nt539GIfOiDBAPdMLtRwKOyKXmI_XazWx1vnJ7NilrKg5xYGvgLDiHCL3W7PefHR79CXGGtLImAfPDCSxV7tFrs=s90', 'name': 'My Favorite Buttermilk Biscuits'})
+    foods.append({'flavors': [0.16666666666666666, 0.8333333333333334, 0.16666666666666666, 0.8333333333333334, 0.0, 0.3333333333333333], 'image': 'http://lh3.ggpht.com/n-TjpfGs5W2c_l79Zo1RlLd-E1G6Gt452FBMSzSM1LyzhpJx59FB-dm_8XJkF1fz4exN1Csbk9h0pr_6GJgK-lM=s90', 'name': 'Garlic Knots'}) 
+    foods.append({'flavors': [0.6666666666666666, 1.0, 0.5, 0.3333333333333333, 0.0, 0.16666666666666666], 'image': 'http://lh6.ggpht.com/0CPJQy4X-doRS9VtZaMrI2HkLGMCaNwUqlqM5cxrPNBfwFWoFgqgPzUMUz0DEAvy821aRixZ5bjjrEB-Yip_0Lc=s90', 'name': 'Dead Simple Slaw'})
+    foods.append({'flavors': [0.5, 0.3333333333333333, 0.5, 0.5, 0.0, 0.8333333333333334], 'image': 'http://lh3.ggpht.com/daLmPWVvNR466D8m_tKF9uGg7rw6eCFXKKzqprMkoUXHI9faUctEIppYYeHd3ZP0phM7inUj8ZAZLXzqnfbT=s90', 'name': '5 Minute Homemade Mac and Cheese'})
+    attribution="Recipe search powered by <a href='http://www.yummly.com/recipes'><img alt='Yummly' src='http://static.yummly.com/api-logo.png'/></a>"
+    if request.method == "GET":
+        return render_template("base.html", foods=foods, attribution=attribution)
+    else:
+        return render_template("base.html", foods=foods, attribution=attribution)
 
 
 if __name__=="__main__":
--- randomfoods.py
+++ randomfoods.py
@@ -4,34 +4,65 @@ import urllib2, json, random
 app=Flask(__name__)
 
 def randomfoods(i):
-    foods = []
-    values = []
     result = ""
+    foods = []
+    for n in range(1):
+        foods+=randomfoodshelper(i)
+    for food in foods:
+        result+=food['name']
+        result+='<img src="'
+        result+=str(food['image'])
+        result+='"></img>'
+        result+=str(food['distance'])        
+        result+="<br>"
+    result+=str(foods)
+    return result
+    #result+=str(tuple(values))
+    #attribution=(results['attribution']['html'])
+
+def randomfoodshelper(i):
+    foods = []
+    bounds = []
+    values = []
     random.seed()
     random.random()
     random.random()
     for n in range(6):
         r = random.random()
-        values.append(r-0.3)
-        values.append(r+0.3)
-    url = "http://api.yummly.com/v1/api/recipes?_app_id=9bb0bd30&_app_key=9e7a1eeeae374a6f14d388e755204848&flavor.sweet.min=%i&flavor.sweet.max=%i&flavor.piquant.min=%i&flavor.piquant.max=%i&flavor.salty.min=%i&flavor.salty.max=%i&flavor.meaty.min=%i&flavor.meaty.max=%i&flavor.sour.min=%i&flavor.sour.max=%i&flavor.bitter.min=%i&flavor.bitter.max=%i&requirePictures=true" % tuple(values)
+        values.append(r)
+        bounds.append(r-0.4)
+        bounds.append(r+0.4)
+    url = "http://api.yummly.com/v1/api/recipes?_app_id=9bb0bd30&_app_key=9e7a1eeeae374a6f14d388e755204848&flavor.sweet.min=%i&flavor.sweet.max=%i&flavor.piquant.min=%i&flavor.piquant.max=%i&flavor.salty.min=%i&flavor.salty.max=%i&flavor.meaty.min=%i&flavor.meaty.max=%i&flavor.sour.min=%i&flavor.sour.max=%i&flavor.bitter.min=%i&flavor.bitter.max=%i&requirePictures=true&maxresult=10" % tuple(bounds)
     request = urllib2.urlopen(url)
     res_string = request.read()
     results = json.loads(res_string)
+    print results
     for match in results['matches']:
+        thisfood = {}
+        distance = 0
         if 'smallImageUrls' in match.keys():
-            foods+=(match['smallImageUrls'])
-    for food in foods:
-        result+='<img src="'
-        result+=food
-        result+='"></img>'
-    result+=str(tuple(values))
-    attribution=(results['attribution']['html'])
-    return result
+            thisfood['image'] = (match['smallImageUrls'][0])
+        if 'flavors' in match.keys():
+            distance += ((match['flavors']['sweet']-values[0])**2)+((match['flavors']['piquant']-values[1])**2)+((match['flavors']['salty']-values[2])**2)
+            distance += ((match['flavors']['meaty']-values[3])**2)+((match['flavors']['sour']-values[4])**2)+((match['flavors']['bitter']-values[5])**2)
+            thisfood['distance'] = distance
+            thisfood['flavors']=[0,0,0,0,0,0]
+            thisfood['flavors'][0] = (match['flavors']['sweet'])
+            thisfood['flavors'][1] = (match['flavors']['sour'])
+            thisfood['flavors'][2] = (match['flavors']['salty'])
+            thisfood['flavors'][3] = (match['flavors']['bitter'])
+            thisfood['flavors'][4] = (match['flavors']['piquant'])
+            thisfood['flavors'][5] = (match['flavors']['meaty'])
+        if 'recipeName' in match.keys():
+            thisfood['name'] = (match['recipeName'])
+        
+        foods.append(thisfood)
+    return sorted(foods)[:i]
+    #return foods
 
 @app.route("/")
 def index():
-    return randomfoods(5)
+    return randomfoods(2)
 
 if __name__=="__main__":
    app.debug=True
